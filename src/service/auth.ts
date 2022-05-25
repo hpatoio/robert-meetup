517dcc0d9c2cfc1fa8f1c3706973c4abd35dfb70
Robert Casanova <robb.casanova@gmail.com>
2022-05-25 18:29:13 +0200

    fix: admin secret check

--- src/service/auth.ts
+++ src/service/auth.ts
@@ -1,11 +1,17 @@
 import { Request, Response, NextFunction } from "express";
 
+const { ADMIN_SECRET } = process.env;
+
+if (!ADMIN_SECRET) {
+  throw Error("ADMIN_SECRET must be defined!");
+}
+
 export const onlyAdmins = (
   request: Request,
   response: Response,
   next: NextFunction
 ) => {
-  if (request.headers.authorization !== process.env.ADMIN_SECRET) {
+  if (request.headers.authorization !== `Bearer ${ADMIN_SECRET}`) {
     response
       .status(403)
       .json({ message: "No enough privileges to access endpoint" });
