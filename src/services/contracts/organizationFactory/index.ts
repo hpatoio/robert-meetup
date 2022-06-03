e57371bacb5947e6d1dc42711bd8e5bce68a1b3a
Robert Casanova <robb.casanova@gmail.com>
2022-06-03 12:12:28 +0200

    feat: update add organization contract logic

---
+++ src/services/contracts/organizationFactory/index.ts
@@ -0,0 +1,46 @@
+import { assertDefined } from "../../../utils/invariants";
+import web3 from "../index";
+import organizationFactoryABI from "./abi/organizationFactory";
+
+const { ORGANIZATION_FACTORY_ADDRESS } = process.env;
+
+assertDefined(
+  ORGANIZATION_FACTORY_ADDRESS,
+  "ORGANIZATION_FACTORY_ADDRESS must be defined!"
+);
+
+let organizationFactoryContract;
+
+// IIFE required because with commonjs modules export is not possible to use await top-level:
+// https://v8.dev/features/top-level-await
+(async () => {
+  const [from] = await web3.eth.getAccounts();
+
+  organizationFactoryContract = new web3.eth.Contract(
+    organizationFactoryABI,
+    ORGANIZATION_FACTORY_ADDRESS,
+    { from }
+  );
+})();
+
+interface Params {
+  name: string;
+  organizers: string[];
+}
+
+export const createOrganization = async ({ name, organizers }: Params) => {
+  const receipt = await organizationFactoryContract.methods
+    .createOrganization(name, organizers, [50, 50])
+    .send()
+    .on("sending", () => console.log("Sending TX.."))
+    .on("transactionHash", (hash) =>
+      console.log(`Sent: https://mumbai.polygonscan.com/tx/${hash}`)
+    )
+    .on("receipt", ({ events }) =>
+      console.log(
+        `Processed: https://mumbai.polygonscan.com/address/${events.NewOrganization.returnValues.newOrganization}`
+      )
+    );
+
+  return receipt.events.NewOrganization.returnValues.newOrganization;
+};
