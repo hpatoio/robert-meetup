c49cb15ca8efeea237f36c64f47a61249cd08ed6
Robert Casanova <robb.casanova@gmail.com>
2022-06-07 09:52:53 +0200

    fix: wrong module path

--- src/routes/meetup.ts
+++ src/routes/meetup.ts
@@ -1,7 +1,7 @@
 /** source/routes/posts.ts */
 import express from "express";
 import { getAll, getById, add } from "../controllers/meetup";
-import { onlyAdmins } from "../service/auth";
+import { onlyAdmins } from "../services/auth";
 
 const router = express.Router();
 
