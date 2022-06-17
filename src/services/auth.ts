98bb0c16f6d3e0fb8c512f5c82a31fefa0b84972
Robert Casanova <robb.casanova@gmail.com>
2022-06-17 20:15:15 +0200

    test: add unit tests for onlyAdmins middleware

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,16 +1,14 @@
-import { Request, Response, NextFunction } from "express";
+import type { Request, Response, NextFunction } from "express";
 import { assertDefined } from "../utils/invariants";
 
-const { ADMIN_SECRET } = process.env;
-
-assertDefined(ADMIN_SECRET, "ADMIN_SECRET must be defined!");
+assertDefined(process.env.ADMIN_SECRET, "ADMIN_SECRET must be defined!");
 
 export const onlyAdmins = (
   request: Request,
   response: Response,
   next: NextFunction
 ) => {
-  if (request.headers.authorization !== `Bearer ${ADMIN_SECRET}`) {
+  if (request.headers.authorization !== `Bearer ${process.env.ADMIN_SECRET}`) {
     response
       .status(403)
       .json({ message: "No enough privileges to access endpoint" });
